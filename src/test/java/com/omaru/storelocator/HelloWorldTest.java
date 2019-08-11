f019b1c2716bec15864c4a905c3f544d318fe47a
omaru <omarux@gmail.com>
2019-08-10 23:43:12 -0600

    initialise project

---
+++ src/test/java/com/omaru/storelocator/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.omaru.storelocator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HelloWorldTest{
+	@Test	
+	void junitFiveShouldExecute(){
+		assertTrue(true);
+	}
+}
