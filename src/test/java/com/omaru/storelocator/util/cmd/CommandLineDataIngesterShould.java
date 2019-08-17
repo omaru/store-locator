e5ae15966c8e0134d6f02f21849b35cc500823f3
omaru <omarux@gmail.com>
2019-08-16 21:48:01 -0600

    commandline test

--- src/test/java/com/omaru/storelocator/util/cmd/CommandLineDataIngesterShould.java
+++ src/test/java/com/omaru/storelocator/util/cmd/CommandLineDataIngesterShould.java
@@ -1,11 +1,16 @@
 package com.omaru.storelocator.util.cmd;
 
 import com.omaru.storelocator.util.script.ScriptRunner;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CommandLineDataIngesterShould {
@@ -20,6 +25,10 @@ public class CommandLineDataIngesterShould {
     }
     @Test
     void onFileRoutePresentRunFromFile() throws Exception {
+        CommandLine.Builder builder = new CommandLine.Builder();
+        builder.addOption(new Option("i","")).addArg("/myFile");
+        CommandLine cmd =builder.build();
+        when(commandLineMenu.getCommandLine(any())).thenReturn(cmd);
         commandLineDataIngester.accept("test");
     }
 }
